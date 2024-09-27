067559a85b0d5b6308c676f5ac5ca5351070bf12
fornt-mokh <mokht489@gmail.com>
2024-09-27 02:24:42 -0700

    s

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -42,6 +42,15 @@ export default function Footer() {
               </li>
             ))}
           </ul>
+
+          <div className="w-full border-t-[1.5px] mt-6 pt-6 border-mid-tone flex justify-center gap-8">
+            <button className="flex items-center justify-center w-12 h-12 rounded-md bg-mid-tone">
+              <Image src={"/icons/instagram.svg"} alt={""} width={32} height={32}/>
+            </button>
+            <button className="flex items-center justify-center w-12 h-12 rounded-md bg-mid-tone">
+            <Image src={"/icons/email.svg"} alt={""} width={32} height={32}/>
+            </button>
+          </div>
         </div>
         <div className="hidden col-span-3 md:grid grid-cols-2">
           <div>
